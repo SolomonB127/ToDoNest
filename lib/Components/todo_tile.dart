ba3cfd4142471a75908b74c5cb51660c037b34b6
SolomonB127 <solomonakinsulire@gmail.com>
2024-08-13 14:21:42 +0100

    Made some style modifications e.g strike through if task is completed

--- lib/Components/todo_tile.dart
+++ lib/Components/todo_tile.dart
@@ -15,7 +15,7 @@ class TodoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 24.0),
       child: Container(
         padding: const EdgeInsets.all(24.0),
         decoration: BoxDecoration(
@@ -23,11 +23,19 @@ class TodoTile extends StatelessWidget {
         child: Row(
           children: [
             // Checkbox
-            Checkbox(value: isTaskCompleted, onChanged: onChanged),
+            Checkbox(
+              value: isTaskCompleted,
+              onChanged: onChanged,
+              activeColor: Colors.black,
+            ),
 
             // Task
             Text(
               taskName,
+              style: TextStyle(
+                  decoration: isTaskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none),
             ),
           ],
         ),
