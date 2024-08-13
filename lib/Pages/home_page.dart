b2b514e31a60948b9cc6b2b353a7d042f2ccaed4
SolomonB127 <solomonakinsulire@gmail.com>
2024-08-13 13:12:28 +0100

    Created an AppBar in HomePage and imported new component -ToDoTile

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:todo_nest/Components/todo_tile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Center(child: Text("To Do")),
+        elevation: 0,
+      ),
+      body: ListView(
+        children: const [
+          TodoTile(),
+        ],
+      ),
+    );
   }
 }
