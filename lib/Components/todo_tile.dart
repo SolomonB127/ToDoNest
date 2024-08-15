bc42bb7a73041cd759224c6db098b6cb211e4c77
SolomonB127 <solomonakinsulire@gmail.com>
2024-08-15 17:46:42 +0100

    Added a new dependency to project - Flutter_Slidable

--- lib/Components/todo_tile.dart
+++ lib/Components/todo_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoTile extends StatelessWidget {
   // Tile Variables
@@ -16,28 +17,30 @@ class TodoTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 24.0),
-      child: Container(
-        padding: const EdgeInsets.all(24.0),
-        decoration: BoxDecoration(
-            color: Colors.blueAccent, borderRadius: BorderRadius.circular(12)),
-        child: Row(
-          children: [
-            // Checkbox
-            Checkbox(
-              value: isTaskCompleted,
-              onChanged: onChanged,
-              activeColor: Colors.black,
-            ),
-
-            // Task
-            Text(
-              taskName,
-              style: TextStyle(
-                  decoration: isTaskCompleted
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none),
-            ),
-          ],
+      child: Slidable(
+        child: Container(
+          padding: const EdgeInsets.all(24.0),
+          decoration: BoxDecoration(
+              color: Colors.blueAccent, borderRadius: BorderRadius.circular(12)),
+          child: Row(
+            children: [
+              // Checkbox
+              Checkbox(
+                value: isTaskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.black,
+              ),
+        
+              // Task
+              Text(
+                taskName,
+                style: TextStyle(
+                    decoration: isTaskCompleted
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none),
+              ),
+            ],
+          ),
         ),
       ),
     );
