5004c2f86987fbe2ca3bb15ed17d77f06adc09d8
SolomonB127 <solomonakinsulire@gmail.com>
2024-08-15 18:57:19 +0100

    Created a data folder which contains a database.dart file that stores default datasets and methods

---
+++ lib/data/database.dart
@@ -0,0 +1,27 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class ToDoDB {
+  //list task
+  List toDoTask = [];
+
+  // Reference the Hive Box
+  final _storageBox = Hive.box('storage');
+
+  // run this if app is opened for the first time ever (default values)
+  void createInitialData() {
+    toDoTask = [
+      ["Make First App", false],
+      ["Eat", false],
+    ];
+  }
+
+  // load Data from Db
+  void loadData() {
+    toDoTask = _storageBox.get("TODOLIST");
+  }
+
+  // Update Data from Db
+  void updateData() {
+    _storageBox.put("TODOLIST", toDoTask);
+  }
+}
