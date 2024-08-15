402ce99787bb99a59dd385a28faec8cac9040c5c
SolomonB127 <solomonakinsulire@gmail.com>
2024-08-15 18:14:36 +0100

    Imported the Slidable Widget in the todo_tile.dart as well as created the deleteTask variable

--- lib/Components/todo_tile.dart
+++ lib/Components/todo_tile.dart
@@ -5,23 +5,35 @@ class TodoTile extends StatelessWidget {
   // Tile Variables
   final String taskName;
   final bool isTaskCompleted;
-  final void Function(bool?)? onChanged;
+  final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteTask;
 
   const TodoTile(
       {super.key,
       required this.taskName,
       required this.isTaskCompleted,
-      required this.onChanged});
+      required this.onChanged,
+      required this.deleteTask});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 24.0),
       child: Slidable(
+        endActionPane:
+            ActionPane(motion: const ScrollMotion(), children: <Widget>[
+          SlidableAction(
+            onPressed: deleteTask,
+            icon: Icons.delete,
+            backgroundColor: Colors.red,
+            borderRadius: BorderRadius.circular(13),
+          )
+        ]),
         child: Container(
           padding: const EdgeInsets.all(24.0),
           decoration: BoxDecoration(
-              color: Colors.blueAccent, borderRadius: BorderRadius.circular(12)),
+              color: Colors.blueAccent,
+              borderRadius: BorderRadius.circular(12)),
           child: Row(
             children: [
               // Checkbox
@@ -30,7 +42,7 @@ class TodoTile extends StatelessWidget {
                 onChanged: onChanged,
                 activeColor: Colors.black,
               ),
-        
+
               // Task
               Text(
                 taskName,
